540a41055187399344718ce5569b198c267f8807
Asma Zubair <zubairasma50@gmail.com>
2026-01-14 20:44:59 +0500

    Time: 9 ms (44.18%), Space: 17.9 MB (23.35%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+           return False
+        
+        number=x
+        newnumber=0
+        while x>0:
+            newnumber= newnumber*10 + x%10
+            x=x//10
+        return newnumber==number
+
+      
+  
