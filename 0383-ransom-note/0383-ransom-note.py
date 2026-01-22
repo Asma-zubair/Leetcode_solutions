889b89e0b582fd6253e66f9913c1c9271a91141f
Asma Zubair <zubairasma50@gmail.com>
2026-01-22 17:12:32 +0500

    Time: 27 ms (28.64%), Space: 19.8 MB (10.15%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        hashmap={}
+        for i in magazine:
+            if i not in hashmap:
+                hashmap[i]=1
+            else:
+                hashmap[i]+=1
+        
+        for r in ransomNote:
+            if r not in hashmap or hashmap[r]==0:
+                return False
+            hashmap[r]-=1
+        return True
+        
+
