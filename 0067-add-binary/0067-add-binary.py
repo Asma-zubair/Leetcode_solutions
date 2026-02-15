aec711cc4521a19789936345bb3da3e641d75e2c
Asma Zubair <zubairasma50@gmail.com>
2026-02-15 23:40:26 +0500

    Time: 0 ms (100%), Space: 17.7 MB (69.49%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,10 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        a_int = int(a, 2)
+        b_int = int(b, 2)
+        
+        # Add the integers
+        sum_int = a_int + b_int
+        
+        # Convert the sum back to a binary string
+        return bin(sum_int)[2:] 
